67a66e2de957b404957a8800d7b1cd5f84c42505
Tutor-DA <support@tutorda.org>
2025-06-20 00:06:23 +0400

    kotlin

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDrhlgmqCC3lmWQ6hmysBY-IG6oEO8Z3P8',
-  authDomain: 'tutorda-mobile-app.firebaseapp.com',
-  projectId: 'tutorda-mobile-app',
-  storageBucket: 'tutorda-mobile-app.appspot.com',
-  messagingSenderId: '11850336257',
-  appId: '1:11850336257:web:8ad996eaaf70a7d2841800',
-  measurementId: 'G-NWGMN7ZG7T',
+  apiKey: "AIzaSyDrhlgmqCC3lmWQ6hmysBY-IG6oEO8Z3P8",
+authDomain: "tutorda-mobile-app.firebaseapp.com",
+projectId: "tutorda-mobile-app",
+storageBucket: "tutorda-mobile-app.firebasestorage.app",
+messagingSenderId: "11850336257",
+appId: "1:11850336257:web:66f849463182888b841800",
+//measurementId: "G-4KQB4XH8TK"
 };
 
 const app = initializeApp(firebaseConfig);
